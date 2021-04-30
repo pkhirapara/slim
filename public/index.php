40f5e84456c80109294d3ed7a39b45800041188e
pratik hirpara <pkhirparapk@outlook.com>
2021-04-30 17:23:20 +0200

    first commit with route and route parameter

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use DI\Container;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$container = new Container();
+
+AppFactory::setContainer($container);
+
+$app = AppFactory::create();
+
+$app->get('/', function(Request $request, Response $response) {
+    $response->getBody()->write("Hello, World!");
+    return $response;
+});
+
+$app->get('/hello/jane', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello, Jane!');
+    return $response;
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = ucfirst($args['name']);
+    $response->getBody()->write(sprintf("Hello, %s!", $name));
+    return $response;
+});
+
+$app->run();
